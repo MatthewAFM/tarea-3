9fd372154c89279ff6e1dd04439401a572559450
MatthewAFM <109297675+MatthewAFM@users.noreply.github.com>
2024-09-08 23:10:10 -0600

    Create perro.py

---
+++ perro.py
@@ -0,0 +1,20 @@
+class Perro(Mammal):
+    def __init__(self, name: str, type: str, head: Head):
+        super().__init__(type)
+        self.name = name
+        self.head = head
+
+    def get_name(self) -> str:
+        return self.name
+
+    def set_name(self, name: str):
+        self.name = name
+
+    def caminar(self):
+        print(f"{self.name} listo para caminar")
+
+    def get_head(self) -> Head:
+        return self.head
+
+    def set_head(self, head: Head):
+        self.head = head
